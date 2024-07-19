8dd95c211bb395a3695a8ef082eb0fa0928459d3
lazzaric92 <lazzaric_92@hotmail.it>
2024-07-19 19:38:49 +0200

    added header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,12 @@
-<header>
-
+<header class="p-3">
+    <nav class="navbar">
+        <ul class="d-flex align-items-center mb-2 fw-bold">
+            <li class="nav-item mx-2">
+                <a class="nav-link" aria-current="page" href="/">Home</a>
+            </li>
+            <li class="nav-item mx-2">
+                <a class="nav-link" href="{{route('trains.index')}}">Trains</a>
+            </li>
+        </ul>
+    </nav>
 </header>
